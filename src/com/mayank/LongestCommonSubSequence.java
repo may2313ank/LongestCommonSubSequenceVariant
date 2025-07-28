60e80c6231d045923b340e91f9c71dea04d7e889
Mayank Gupta <mayankgupta9450@gmail.com>
2025-07-28 09:39:03 -0700

    Update LongestCommonSubSequence.java

--- src/com/mayank/LongestCommonSubSequence.java
+++ src/com/mayank/LongestCommonSubSequence.java
@@ -18,7 +18,7 @@ public class LongestCommonSubSequence {
 	  for(int i=0;i<=m;i++) {
 		  for(int j=0;j<=n;j++) {
 			  if(i == 0 || j==0) {
-				  dp[i][j]=0;
+			   	dp[i][j]=0;
 			  }
 		  }
 	  }
